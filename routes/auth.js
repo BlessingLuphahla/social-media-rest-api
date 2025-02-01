5f366e679b557cf86dd38896b26643c2b02f1f3b
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 15:38:46 +0200

    auth update

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
   const newUser = await new User({
@@ -9,6 +10,8 @@ router.post("/register", async (req, res) => {
   });
 
   try {
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
     await newUser.save().then(() => {
       res.status(200).json("User has been created");
     });
