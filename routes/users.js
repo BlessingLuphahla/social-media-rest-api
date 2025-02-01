b4ad1d0f8970d6eb2acba0ba35663fd3c339d44e
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 19:21:14 +0200

    users.js update

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.put("/:id", async (req, res) => {
       }
     }
     try {
-      const user = User.findByIdAndUpdate(req.params.id, {
+      const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
       res.status(200).json("Account has been updated");
@@ -48,6 +48,34 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//get a user
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//delete a user
+
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can delete only your account!");
+  }
+});
+
 
 //follow a user
 
