8f1b44d2295e028ad6530ddade3b1b5e20280ad0
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 15:19:12 +0200

    auth update

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const User = require("../models/User");
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.post("/register", async (req, res) => {
+  const newUser = await new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    await newUser.save().then(() => {
+      res.status(200).json("User has been created");
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+// router.post("/login", (req, res) => {
+//   res.send("Login");
+// });
 
 module.exports = router;
