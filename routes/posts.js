d121f683120106da33dc9e2732df4fe78699cff9
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 22:46:23 +0200

    posts update

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,3 @@
-const { route } = require("./users");
-
 const router = require("express").Router();
 const Post = require("../models/Post");
 
@@ -14,6 +12,23 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// update a post
+
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can update only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 // delete a post
 
