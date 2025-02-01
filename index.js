6ad0fd97409df8ee60f431dfb2188850aca422ce
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 12:36:21 +0200

    index users update

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const UserRouter = require('./routes/users');
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+},
+).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+// middleweares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use('/api/users', UserRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -14,6 +30,3 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
-
-
