ece68ef0389ad2937b2e19f785654ce09246b8d7
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 17:13:15 +0200

    users.js update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,73 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+// update user
+
+router.put('/:id', (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = bcrypt.genSaltSync(10);
+                req.body.password = bcrypt.hashSync(req.body.password, salt);
+            } catch (err) {
+                return res.status(500).json(err);
+            }
+        }
+        try {
+            const user = User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json('Account has been updated');
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json('You can update only your account!');
+    }
 });
+
+//create user
+
+router.post('/', async (req, res) => {
+    const user = await User.findOne({ username: req.body.username });
+    if (user) {
+        return res.status(400).json('User already exists!');
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); 
+
+//follow a user
+
+router.put('/:id/follow', async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
+                res.status(200).json('user has been followed');
+            } else {
+                res.status(403).json('you already follow this user');
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json('you cant follow yourself');
+    }
+});
+
 
 module.exports = router;
