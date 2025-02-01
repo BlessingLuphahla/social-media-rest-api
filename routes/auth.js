9b2edba9b4405faff15bc4cb8d51c9e88923e478
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-01 17:07:06 +0200

    auth update

--- routes/auth.js
+++ routes/auth.js
@@ -20,8 +20,19 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// router.post("/login", (req, res) => {
-//   res.send("Login");
-// });
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).json("Wrong username!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) return res.status(400).json("Wrong password!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
