8eea03b3af1b7310c2d62ae096c9d295b9ce6f13
REDD AXE <160331096+BlessingLuphahla@users.noreply.github.com>
2025-02-05 17:03:32 +0200

    update

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const UserRouter = require("./routes/users");
+const AuthRouter = require("./routes/auth");
+const PostRouter = require("./routes/posts");
+const cors = require("cors");
 
-const morgan = require('morgan');
-const UserRouter = require('./routes/users');
-const AuthRouter = require('./routes/auth');
-const PostRouter = require('./routes/posts');
-
-
-const app = express();
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
-},
-).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+console.log(process.env);
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected!");
 });
 
 // middleweares
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
 // setting up APIs
 
-app.use('/api/users', UserRouter);
-app.use('/api/auth', AuthRouter);
-app.use('/api/posts', PostRouter);
+app.use("/api/users", UserRouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/posts", PostRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+process.on("SIGINT", async () => {
+    await mongoose.connection.close();
+    console.log("MongoDB connection closed.");
+    process.exit(0);
 });
